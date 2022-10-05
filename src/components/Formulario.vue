2ba7ca0b42ebbc377d53a2e04d5244f83dab35c5
Dianaandrade2022 <a3521110157@alumno.uttehuacan.edu.mx>
2022-10-04 20:00:29 -0500

    first commit

---
+++ src/components/Formulario.vue
@@ -0,0 +1,37 @@
+<template>
+<!-- bootstrap form -->
+<form @submit.prevent="registrarproyecto">
+    <div class="mb-3">
+      <label for="" class="form-label">Proyecto</label>
+      <input type="text" class="form-control" required>
+    </div>
+    <div class="mb-3">
+        <label for="" class="form-label">Actividad</label>
+        <select class="form-select" required>
+            <option disabled selected value="">Seleccione tipo de actividad</option>
+            <option value="1">Aplicaciones Web con Vue.JS</option>
+            <option value="2">Backend Service con Node.js</option>
+            <option value="3">App Movil con React Native</option>
+        </select>
+    </div>
+    <div class="mb-3">
+        <label class="form-check-label m-1">Urgente</label>
+        <input type="checkbox" class="form-check-input m-2">
+    </div>
+    <button type="submit" class="btn btn-primary">Guardar</button>
+  </form>
+</template>
+
+<script>
+    export default{
+      data:()=>({
+        
+      }),
+      methods:{
+        registrarproyecto(){
+          console.log("Proyecto registrado ...")
+        }
+      },
+    };
+  
+  </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+        <div class="row">
+          <div class="col-12 col-md-6">
+            <h1>App - Proyectos</h1>
+          
+          </div>
+          <div class="col-12 col-md-6">
+            <img width="100" :src="url" :alt="des">
+          </div>
+          <hr>
+        </div>
+</template>
+<script>
+    
+    export default{
+      data:()=>({
+        url:"https://cdn.svgporn.com/logos/vue.svg",
+        des:"Vue.Js",
+      }),
+    };
+  
+  </script>
