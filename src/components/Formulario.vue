8e7bc23ddf22993e01c98e32be9fd74503ed8106
Dianaandrade2022 <a3521110157@alumno.uttehuacan.edu.mx>
2022-10-10 21:33:26 -0500

    actualización

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,55 +1,64 @@
 <template>
-<!-- bootstrap form -->
+
+  <div class="row">
+    <div class="col-12 col-md-4">
+      <h3 class="text-center" :class="proyecto.urgente ? 'text-success' : 'text-info' "   >Progreso {{porcentaje}}</h3>
+
+      <!-- progress bar -->
+      <div class="progress">
+        <div class="progress-bar progress-bar-striped progress-bar-animated bg-success"
+        role="progressbar"
+        :aria-valuenow="porcentaje"
+        aria-valuemin="0"
+        aria-valuemax="100"
+        :style="`width:${porcentaje}%`">
+
+        </div>
+      </div>
+       <!-- progress bar -->
+
+      <!-- bootstrap form -->
 <form @submit.prevent="registrarproyecto">
-    <div class="mb-3">
-      <label for="" class="form-label">Proyecto</label>
-      <input v-model.trim="proyecto" type="text" class="form-control" required>
+  <div class="mb-3">
+    <label for="" class="form-label">Proyecto</label>
+    <input v-model.trim="proyecto" type="text" class="form-control" required>
+  </div>
+  <div class="mb-3">
+      <label for="" class="form-label">Actividad</label>
+      <select class="form-select" required  v-model.trim="tipo">
+          <option disabled selected value="">Seleccione tipo de actividad</option>
+          <option value="1">Aplicaciones Web con Vue.JS</option>
+          <option value="2">Backend Service con Node.js</option>
+          <option value="3">App Movil con React Native</option>
+      </select>
+  </div>
+  <div class="mb-3">
+      <label class="form-check-label m-1">Urgente</label>
+      <input type="checkbox" class="form-check-input m-2" v-model.trim="urgente">
+  </div>
+  <button type="submit" class="btn btn-primary">Guardar</button>
+</form>
+
     </div>
-    <div class="mb-3">
-        <label for="" class="form-label">Actividad</label>
-        <select class="form-select" required  v-model.trim="tipo">
-            <option disabled selected value="">Seleccione tipo de actividad</option>
-            <option value="1">Aplicaciones Web con Vue.JS</option>
-            <option value="2">Backend Service con Node.js</option>
-            <option value="3">App Movil con React Native</option>
-        </select>
+    <div class="col-12 col-md-8">
+      <total-proyectos :numeroProyectos="numeroProyectos" :proyectos="proyectos" :cambiarEstado="cambiarEstado"/>
     </div>
-    <div class="mb-3">
-        <label class="form-check-label m-1">Urgente</label>
-        <input type="checkbox" class="form-check-input m-2" v-model.trim="urgente">
-    </div>
-    <button type="submit" class="btn btn-primary">Guardar</button>
-  </form>
-  <hr>
-  <div class="table-responsive">
-    <table class="table table-dark text-center">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Proyecto</th>
-                <th>Tipo</th>
-                <th>Urgente</th>
-            </tr>
-        </thead>
-        <tbody v-for="(proyecto,index) in proyectos" :key="index">
-            <tr class="table-light">
-                <th>{{index + 1}}</th>
-                <td>{{proyecto.proyecto}}</td>
-                <td>{{proyecto.tipo}}</td>
-                <td class="font-weight-bold" :class="proyecto.urgente ? 'table-danger text-danger' : 'table-success text-success'" role="alert">{{proyecto.urgente ? "Si" : "No"}}</td>
-            </tr>
-        </tbody>
-    </table>
+  
   </div>
 </template>
 
 <script>
+  import TotalProyectos from './TotalProyectos.vue';
     export default{
+      components:{
+        TotalProyectos
+      },
       data:()=>({
         proyecto:"",
         tipo:"",
         urgente:false,
-        proyectos:[]
+        proyectos:[],
+        numeroProyectos:0,
       }),
       methods:{
         registrarproyecto(){
@@ -58,14 +67,35 @@
                 tipo : this.tipo,
                 urgente : this.urgente
             };
-
-        this.proyectos.push(proyecto);
+          
+        this.proyectos.push(proyecto),
         this.proyecto = "",
         this.tipo = "",
         this.urgente = false,
           console.log(this.proyectos)
+        },
+        cambiarEstado(id){
+          //this.urgente = !this.urgente;
+          this.proyectos[id].urgente = !this.proyectos[id].urgente;
+        }
+
+      },
+      computed:{
+        numeroProyectos(){
+          return this.proyectos.length;
+        },
+        porcentaje(){
+          let completados = 0;
+          
+          this.proyectos.map(proyectos =>{
+            if (proyectos.i) {
+              completados++;
+            }
+            return (completados *100) / this.numeroProyectos || 10;
+          });
         }
       },
+      
     };
   
   </script>
---
+++ src/components/TotalProyectos.vue
@@ -0,0 +1,36 @@
+<template>
+    <hr>
+    <h3 class="text-center">Total de proyectos : {{this.proyectos.length}}</h3>
+    <div class="table-responsive">
+      <table class="table table-dark text-center">
+          <thead>
+              <tr>
+                  <th>#</th>
+                  <th>Proyecto</th>
+                  <th>Tipo</th>
+                  <th>Urgente</th>
+              </tr>
+          </thead>
+          <tbody v-for="(proyecto,index) in proyectos" :key="index">
+              <tr class="table-light">
+                  <th>{{index + 1}}</th>
+                  <td>{{proyecto.proyecto}}</td>
+                  <td>{{proyecto.tipo}}</td>
+                  <td @click="cambiarEstado(index)"
+                  class="font-weight-bold" :class="proyecto.urgente ? 'table-danger text-danger' : 'table-success text-success'" role="alert">{{proyecto.urgente ? "Si" : "No"}}</td>
+                  
+              </tr>
+          </tbody>
+      </table>
+    </div>
+</template>
+<script>
+
+export default{
+    props:[
+        'numeroProyectos',
+        'proyectos',
+        'cambiarEstado'
+    ],
+}
+</script>
